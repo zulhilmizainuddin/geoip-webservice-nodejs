8e3e96acfcaa458c16a332222c4362ead8e50723
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2016-10-26 18:51:44 +0800

    Initialize ip2location databases only once each to improve performance

---
+++ models/ip2location.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const ip2locationIPv4 = require('ip2location-nodejs');
+const ip2locationIPv6 = require('ip2location-nodejs');
+const path = require('path');
+const net = require('net');
+
+const config = require('../config');
+const logger = require('../utils/logger');
+
+class IP2Location {
+    constructor() {
+        this.isIPv4DBInitialized = false;
+        this.isIPv6DBInitialized = false;
+    }
+
+    query(ip) {
+        let result = null;
+
+        if (net.isIPv4(ip)) {
+            if (!this.isIPv4DBInitialized) {
+                const databasePath = path.join(__dirname, `../databases/${config.ipv4db}`);
+                ip2locationIPv4.IP2Location_init(databasePath);
+
+                logger.info('IP2Location IPv4 database initialized');
+                this.isIPv4DBInitialized = true;
+            }
+
+            result = ip2locationIPv4.IP2Location_get_all(ip);
+        }
+
+        if (net.isIPv6(ip)) {
+            if (!this.isIPv6DBInitialized) {
+                const databasePath = path.join(__dirname, `../databases/${config.ipv6db}`);
+                ip2locationIPv4.IP2Location_init(databasePath);
+
+                logger.info('IP2Location IPv6 database initialized');
+                this.isIPv6DBInitialized = true;
+            }
+
+            result = ip2locationIPv6.IP2Location_get_all(ip);
+        }
+
+        return result;
+    }
+}
+
+const ip2location = new IP2Location();
+
+module.exports = ip2location;
--- routes/ip2location-controller.js
+++ routes/ip2location-controller.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const IP2Location = require('../models/IP2Location');
+const ip2location = require('../models/ip2location');
 
 router.get('/', function(req, res, next) {
-    const ip2Location = new IP2Location();
-    const queriedData = ip2Location.query(req.query.ipaddress);
+    const queriedData = ip2location.query(req.query.ipaddress);
 
     let geoLocation;
     if (queriedData !== null) {
