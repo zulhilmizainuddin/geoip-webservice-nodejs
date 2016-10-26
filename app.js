1763fbcdd69f00ed0a39250f1fad9dbffaa31b9f
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2016-10-26 19:55:00 +0800

    Restore removed dependencies

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 
 const express = require('express');
 const path = require('path');
+const favicon = require('serve-favicon');
 const logger = require('morgan');
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const compression = require('compression');
 
@@ -10,9 +12,17 @@ const ip2locationController = require('./routes/ip2location-controller');
 
 const app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(compression());
 
 app.use('/ip2location', ip2locationController);
