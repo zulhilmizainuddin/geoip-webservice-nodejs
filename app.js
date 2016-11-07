7d5d5bfb1993aefea8cd741aaf26a0a416238e91
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2016-11-08 05:42:02 +0800

    Disable unused middleware

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
+// const favicon = require('serve-favicon');
 const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
+// const cookieParser = require('cookie-parser');
+// const bodyParser = require('body-parser');
 const compression = require('compression');
 
 const ip2locationController = require('./routes/ip2location-controller');
@@ -19,10 +19,10 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: false}));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 app.use(compression());
 
 app.use('/ip2location', ip2locationController);
